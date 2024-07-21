72ea0096cd5a21ae3d80b044c6bac29fa2d54c71
Roman Velnar <roman.velnar@gmail.com>
2024-07-21 17:04:08 +0100

    Added code for the new module

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42e7ada7b1d0075bcb2f3415ea54b51d8e0c6149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e711bd19224023bb5d73d217bd07c01a99872d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
